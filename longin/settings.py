1247449915267acd5928efc763c364d75b0b8829
cecil <cecilghimire@gmail.com>
2022-12-02 21:39:00 +0545

    simplejwt setup

--- longin/settings.py
+++ longin/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'accounts',
+    'djangorestframework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+      
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+ 
+}
