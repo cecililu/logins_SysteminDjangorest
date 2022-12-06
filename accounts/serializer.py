a30ec007f3ce92b14ac970005f23e281420819ba
cecil <cecilghimire@gmail.com>
2022-12-06 19:12:00 +0545

    edit in registarion serializer

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -3,6 +3,14 @@ from accounts.models import MyUser
 
 
 class UserSerializer(serializers.Serializer):
+    password2 =serializers.CharField(style={
+        'input_type':'password',
+        'write_only':True
+                  })
+    
     class Meta:
         model = MyUser
-        fields = ('username', 'email', 'id')
+        fields = ('email','name','password','password2','tc')
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 
 from . views import *
 
+
 urlpatterns = [
     path('register',UserRegistationView.as_view())
 ]
